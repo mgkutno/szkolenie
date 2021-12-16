2561d63707b0092496c948981b13c55d824ea4b6
mgkutno <mgkutno@gmail.com>
2021-12-16 09:10:09 +0100

    ver1

---
+++ zadanie_2_1.py
@@ -0,0 +1,12 @@
+#tuple
+x = (1,2,3,4,5,6,7,8,9,11)
+print (x[1])
+print (x[2:7])
+print (x[::7])
+#lista
+lista = [1, 2]
+lista.append(3)
+print (lista)
+print (lista.index(4,2))
+##range
+range(10) #zawiera ele
